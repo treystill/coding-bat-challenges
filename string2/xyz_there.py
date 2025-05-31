1ea59667b6d358af34fb8477574250b7698281f7
Trey <treystill16@gmail.com>
2025-05-31 12:32:36 -0400

    added answer to xyz there

--- string2/xyz_there.py
+++ string2/xyz_there.py
@@ -1,13 +1,6 @@
 def xyz_there(str):
-  a = 0
-  b = 0
   for index in range(len(str) - 1):
-    if len(str) >= 3:
-      if str[index: index + 3] == "xyz":
-        a = a + 1
-    if str[index: index + 4] == ".xyz":
-      b = b + 1
-      a = a - 1
-  if len(str) >= 3 and a > 0 and a >= b:
-    return True
+    if str[index: index + 3] == 'xyz':
+      if str[index - 1] != '.':
+        return True
   return False
